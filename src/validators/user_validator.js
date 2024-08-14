1865d84c1b9b9742ff3b8c02c06a593b7469cdc7
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2024-08-14 17:09:50 +0300

    updated the validator of the user

--- src/validators/user_validator.js
+++ src/validators/user_validator.js
@@ -1,23 +1,13 @@
 const joi = require("joi");
 const schema = joi.object({
-  firstName: joi
+  firstName: joi.string().min(3).required(),
+  lastName: joi.string().min(3).required(),
+  role: joi.string().required(),
+  age: joi.number().required(),
+  email: joi
     .string()
-    .min(3)
     .required()
-    .message({ msg: "First name is required." }),
-  lastName: joi
-    .string()
-    .min(3)
-    .required()
-    .message({ msg: "Last name is required." }),
-  role: joi.string().required().message({ msg: "the user role is required." }),
-  age: joi.number().required().message({ msg: "user age is required." }),
-  email: joi.string().required().message({ msg: "Email is required." }),
-  password: joi
-    .string()
-    .min(8)
-    .alphanum()
-    .required()
-    .message({ msg: "password must be atleast 8 charachters" }),
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }),
+  password: joi.string().min(8).required(),
 });
 module.exports = schema;

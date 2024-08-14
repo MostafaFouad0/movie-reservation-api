cc1184e2efb2448a8160ff1d46fa2c59fda65367
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2024-08-14 17:12:24 +0300

    implemented the controllers for two of user's endpoints (login,signup).

---
+++ src/routes/controllers/user_controller.js
@@ -0,0 +1,55 @@
+const { PrismaClient } = require("@prisma/client");
+const { generateToken } = require("../../auth/jwt_auth");
+const prisma = new PrismaClient();
+const {
+  formatErrorToJSend,
+  formatSuccessToJSend,
+  formatFailToJSend,
+} = require("../../apiResponsesTemplates/templates");
+const sigupUser = async (req, res) => {
+  const newUserData = req.body;
+  let user = await prisma.user.findFirst({
+    where: {
+      email: newUserData.email,
+    },
+  });
+  // check if user's email already exists
+  if (user) {
+    return res.status(400).json(formatFailToJSend("Email already exists."));
+  }
+  // storing the user's info in the database
+  user = await prisma.user.create({
+    data: newUserData,
+  });
+  return res
+    .status(200)
+    .json(formatSuccessToJSend("user created successfully.", user));
+};
+const loginUser = async (req, res) => {
+  const userCredentials = req.body;
+  if (!userCredentials.email) {
+    return res
+      .status(400)
+      .json(formatFailToJSend("email address is required."));
+  }
+  if (!userCredentials.password) {
+    return res.status(400).json(formatFailToJSend("password is required."));
+  }
+  const user = await prisma.user.findFirst({
+    where: {
+      email: userCredentials.email,
+    },
+  });
+  if (!user || !user.active || userCredentials.password != user.password) {
+    return res
+      .status(400)
+      .json(formatFailToJSend(" invalid email or password."));
+  }
+
+  const Payload = { id: user.id, role: user.role, email: user.email };
+  // return the access token to the clinet
+  res
+    .status(200)
+    .json(formatSuccessToJSend("all data are valid", generateToken(Payload)));
+};
+module.exports = { sigupUser, loginUser };

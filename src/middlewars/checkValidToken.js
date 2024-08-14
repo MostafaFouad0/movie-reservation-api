c41c8a91ed0261b1e0fdef902d0e5b86c03f4bf6
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2024-08-14 17:08:49 +0300

    implemented a function to check if a JWT token is valid or not

---
+++ src/middlewars/checkValidToken.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const { formatFailToJSend } = require("../apiResponsesTemplates/templates");
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  // check if there is no token
+  if (token == null)
+    return res.status(401).json(formatFailToJSend("access token is required."));
+  // verifying token
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err)
+      return res
+        .status(403)
+        .json(formatFailToJSend("access denied due to expired token"));
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = { checkToken };

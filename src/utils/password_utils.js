84e91840858108191c8fb85ed8596db42d198f19
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2024-08-15 08:02:16 +0300

    added function to hash and check if a hash is for a specific password using bcrypt library

---
+++ src/utils/password_utils.js
@@ -0,0 +1,16 @@
+const bcrypt = require("bcrypt");
+
+async function hashPassword(password) {
+  const hash = await bcrypt.hash(password, 10);
+  return hash;
+}
+
+async function checkPassword(password, hash) {
+  const match = await bcrypt.compare(password, hash);
+  return match;
+}
+
+module.exports = {
+  hashPassword,
+  checkPassword,
+};

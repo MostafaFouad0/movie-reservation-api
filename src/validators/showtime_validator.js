75a8aad7a0fce18bdfec2caade78b1931a273126
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2024-08-19 13:44:39 +0300

    fixed a bug when validating the showtime_start

--- src/validators/showtime_validator.js
+++ src/validators/showtime_validator.js
@@ -2,8 +2,8 @@ const joi = require("joi");
 const schema = joi.object({
   hall_id: joi.number().required(),
   movie_id: joi.number().required(),
-  showtime_start: joi.date().required(),
-  showtime_end: joi.date().required(),
+  showtime_start:joi.string().regex(/^([0-9]{2})\:([0-9]{2})$/).required(),
+  showtime_end: joi.string().regex(/^([0-9]{2})\:([0-9]{2})$/).required(),
   vip_seat_price: joi.number().required(),
   regular_seat_price: joi.number().required(),
 });

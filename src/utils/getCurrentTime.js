93bf11a6c3cfef6858d8d80650e10e070cdc5721
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2024-08-26 05:38:31 +0300

    added util functions to get the current date and time in a specific format

---
+++ src/utils/getCurrentTime.js
@@ -0,0 +1,9 @@
+const getCurrentTime = () => {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${hours}:${minutes}`;
+};
+
+module.exports = { getCurrentTime };

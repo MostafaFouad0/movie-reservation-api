e6dcfef2265f7b58906ac41807ba1c1fa864c998
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2024-08-31 08:15:52 +0300

    added endpoint to get all reservations the user has made

--- src/routes/controllers/reservations_controllers.js
+++ src/routes/controllers/reservations_controllers.js
@@ -5,10 +5,16 @@ const {
   formatFailToJSend,
 } = require("../../apiResponsesTemplates/templates");
 const { getPayload } = require("../../auth/jwt_auth");
-const { connect } = require("../user/user_routes");
+const { getCurrentDate } = require("../../utils/getCurrentDate");
+const { formatDate } = require("../../utils/removeSlashes");
 const prisma = new PrismaClient();
 
 const makeReservations = async (req, res) => {
+  const currentDate = formatDate(getCurrentDate());
+  const showtimeDate = formatDate(req.query.date);
+  if (showtimeDate < currentDate) {
+    return res.status(400).json(formatFailToJSend("showtime date is invalid"));
+  }
   const ticket = await prisma.ticket.findFirst({
     where: {
       showtime_id: +req.query.showtime_id,
@@ -61,4 +67,46 @@ const makeReservations = async (req, res) => {
     .json(formatSuccessToJSend("seat reserved successfully", newTicket));
 };
 
-module.exports = { makeReservations };
+const getReservations = async (req, res) => {
+  const userId = req.user.id;
+  const tickets = await prisma.ticket.findMany({
+    where: {
+      user_id: userId,
+    },
+    select: {
+      seat_id: true,
+      showtime_date: true,
+      issued_date: true,
+      expired: true,
+      price: true,
+      showtime_relation: {
+        select: {
+          showtime_movie: {
+            select: {
+              title: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const formattedTickets = tickets.map((ticket) => ({
+    seatId: ticket.seat_id,
+    showtimeDate: ticket.showtime_date,
+    issuedDate: ticket.issued_date,
+    expired: ticket.expired,
+    price: ticket.price,
+    movieTitle: ticket.showtime_relation.showtime_movie.title,
+  }));
+  res
+    .status(200)
+    .json(
+      formatSuccessToJSend(
+        "reservations successfully retrieved",
+        formattedTickets
+      )
+    );
+};
+
+module.exports = { makeReservations, getReservations };
--- src/routes/reservations/reservations_routes.js
+++ src/routes/reservations/reservations_routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { checkToken } = require("../../middlewars/checkValidToken");
 const { checkAdminToken } = require("../../middlewars/checkAdminToken");
-const { makeReservations } = require("../controllers/reservations_controllers");
+const { makeReservations,getReservations } = require("../controllers/reservations_controllers");
 
 router.post("/", checkToken, makeReservations);
+router.get('/',checkToken,getReservations);
 
 module.exports = router;
